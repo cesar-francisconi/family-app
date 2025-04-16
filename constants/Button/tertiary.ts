c700dc14159d4f50d6beedcab78d4f17679d9e78
Cesar Francisconi <cesarafpiza@gmail.com>
2025-04-15 22:09:46 -0300

    add button tokens for primary, secondary, and tertiary types

---
+++ constants/Button/tertiary.ts
@@ -0,0 +1,470 @@
+import { BorderRadius } from "../BorderRadius";
+import { Colors } from "../Colors";
+import { Font } from "../Font";
+import { Spacing } from "../Spacing";
+
+import {
+  ViewStyle,
+  TextStyle,
+  StyleProp,
+  StyleSheet,
+} from "react-native";
+
+export type ButtonStyleTokens = {
+  background: StyleProp<ViewStyle>;
+  text: StyleProp<TextStyle>;
+  border: StyleProp<ViewStyle>;
+};
+
+type ButtonVariantSize = {
+  borderRadius: {
+    [borderRadiusKey: string]: ButtonStyleTokens;
+  };
+};
+
+export type ButtonTokens = {
+  variant: {
+    filled: {
+      size: {
+        [sizeKey: string]: ButtonVariantSize;
+      };
+    };
+    outlined: {
+      size: {
+        [sizeKey: string]: ButtonVariantSize;
+      };
+    };
+    text: {
+      size: {
+        [sizeKey: string]: ButtonVariantSize;
+      };
+    };
+  };
+};
+
+export const tertiary: ButtonTokens = {
+  variant: {
+    filled: {
+      size: {
+        // Medium Button
+        medium: {
+          borderRadius: {
+            large: {
+              background: {
+                backgroundColor: Colors.surface.containerHigh,
+                paddingHorizontal: Spacing['2xl'],
+                height: 44,
+                borderRadius: BorderRadius['2xl'],
+                gap: Spacing['lg'],
+              },
+              text: {
+                color: Colors.surface.on,
+                ...StyleSheet.flatten(Font.label.extraLargeProminent),
+              },
+              border: {},
+            },
+            medium: {
+              background: {
+                backgroundColor: Colors.surface.containerHigh,
+                paddingHorizontal: Spacing['2xl'],
+                height: 44,
+                borderRadius: BorderRadius['sm'],
+                gap: Spacing['lg'],
+              },
+              text: {
+                color: Colors.surface.on,
+                ...StyleSheet.flatten(Font.label.extraLargeProminent),
+              },
+              border: {},
+            },
+            small: {
+              background: {
+                backgroundColor: Colors.surface.containerHigh,
+                paddingHorizontal: Spacing['2xl'],
+                height: 44,
+                borderRadius: BorderRadius['xs'],
+                gap: Spacing['lg'],
+              },
+              text: {
+                color: Colors.surface.on,
+                ...StyleSheet.flatten(Font.label.extraLargeProminent),
+              },
+              border: {},
+            },
+            none: {
+              background: {
+                backgroundColor: Colors.surface.containerHigh,
+                paddingHorizontal: Spacing['2xl'],
+                height: 44,
+                borderRadius: BorderRadius['none'],
+                gap: Spacing['lg'],
+              },
+              text: {
+                color: Colors.surface.on,
+                ...StyleSheet.flatten(Font.label.extraLargeProminent),
+              },
+              border: {},
+            },
+          },
+        },
+
+        // Small Button
+        small: {
+          borderRadius: {
+            large: {
+              background: {
+                backgroundColor: Colors.surface.containerHigh,
+                paddingHorizontal: Spacing['xl'],
+                height: 32,
+                borderRadius: BorderRadius['2xl'],
+                gap: Spacing['md'],
+              },
+              text: {
+                color: Colors.surface.on,
+                ...StyleSheet.flatten(Font.label.large),
+              },
+              border: {},
+            },
+            medium: {
+              background: {
+                backgroundColor: Colors.surface.containerHigh,
+                paddingHorizontal: Spacing['xl'],
+                height: 32,
+                borderRadius: BorderRadius['sm'],
+                gap: Spacing['md'],
+              },
+              text: {
+                color: Colors.surface.on,
+                ...StyleSheet.flatten(Font.label.large),
+              },
+              border: {},
+            },
+            small: {
+              background: {
+                backgroundColor: Colors.surface.containerHigh,
+                paddingHorizontal: Spacing['xl'],
+                height: 32,
+                borderRadius: BorderRadius['xs'],
+                gap: Spacing['md'],
+              },
+              text: {
+                color: Colors.surface.on,
+                ...StyleSheet.flatten(Font.label.large),
+              },
+              border: {},
+            },
+            none: {
+              background: {
+                backgroundColor: Colors.surface.containerHigh,
+                paddingHorizontal: Spacing['xl'],
+                height: 32,
+                borderRadius: BorderRadius['none'],
+                gap: Spacing['md'],
+              },
+              text: {
+                color: Colors.surface.on,
+                ...StyleSheet.flatten(Font.label.large),
+              },
+              border: {},
+            },
+          },
+        },
+      },
+    },
+    outlined: {
+      size: {
+        // Medium Button
+        medium: {
+          borderRadius: {
+            large: {
+              background: {
+                backgroundColor: 'transparent',
+                paddingHorizontal: Spacing['2xl'],
+                height: 44,
+                borderRadius: BorderRadius['2xl'],
+                gap: Spacing['lg'],
+              },
+              text: {
+                color: Colors.surface.onVariant,
+                ...StyleSheet.flatten(Font.label.extraLargeProminent),
+              },
+              border: {
+                borderWidth: 1,
+                borderColor: Colors.surface.onVariant,
+              },
+            },
+            medium: {
+              background: {
+                backgroundColor: 'transparent',
+                paddingHorizontal: Spacing['2xl'],
+                height: 44,
+                borderRadius: BorderRadius['sm'],
+                gap: Spacing['lg'],
+              },
+              text: {
+                color: Colors.surface.onVariant,
+                ...StyleSheet.flatten(Font.label.extraLargeProminent),
+              },
+              border: {
+                borderWidth: 1,
+                borderColor: Colors.surface.onVariant,
+              },
+            },
+            small: {
+              background: {
+                backgroundColor: 'transparent',
+                paddingHorizontal: Spacing['2xl'],
+                height: 44,
+                borderRadius: BorderRadius['xs'],
+                gap: Spacing['lg'],
+              },
+              text: {
+                color: Colors.surface.onVariant,
+                ...StyleSheet.flatten(Font.label.extraLargeProminent),
+              },
+              border: {
+                borderWidth: 1,
+                borderColor: Colors.surface.onVariant,
+              },
+            },
+            none: {
+              background: {
+                backgroundColor: 'transparent',
+                paddingHorizontal: Spacing['2xl'],
+                height: 44,
+                borderRadius: BorderRadius['none'],
+                gap: Spacing['lg'],
+              },
+              text: {
+                color: Colors.surface.onVariant,
+                ...StyleSheet.flatten(Font.label.extraLargeProminent),
+              },
+              border: {
+                borderWidth: 1,
+                borderColor: Colors.surface.onVariant,
+              },
+            },
+          },
+        },
+
+        // Small Button
+        small: {
+          borderRadius: {
+            large: {
+              background: {
+                backgroundColor: 'transparent',
+                paddingHorizontal: Spacing['xl'],
+                height: 32,
+                borderRadius: BorderRadius['2xl'],
+                gap: Spacing['md'],
+              },
+              text: {
+                color: Colors.surface.onVariant,
+                ...StyleSheet.flatten(Font.label.large),
+              },
+              border: {
+                borderWidth: 1,
+                borderColor: Colors.surface.onVariant,
+              },
+            },
+            medium: {
+              background: {
+                backgroundColor: 'transparent',
+                paddingHorizontal: Spacing['xl'],
+                height: 32,
+                borderRadius: BorderRadius['sm'],
+                gap: Spacing['md'],
+              },
+              text: {
+                color: Colors.surface.onVariant,
+                ...StyleSheet.flatten(Font.label.large),
+              },
+              border: {
+                borderWidth: 1,
+                borderColor: Colors.surface.onVariant,
+              },
+            },
+            small: {
+              background: {
+                backgroundColor: 'transparent',
+                paddingHorizontal: Spacing['xl'],
+                height: 32,
+                borderRadius: BorderRadius['xs'],
+                gap: Spacing['md'],
+              },
+              text: {
+                color: Colors.surface.onVariant,
+                ...StyleSheet.flatten(Font.label.large),
+              },
+              border: {
+                borderWidth: 1,
+                borderColor: Colors.surface.onVariant,
+              },
+            },
+            none: {
+              background: {
+                backgroundColor: 'transparent',
+                paddingHorizontal: Spacing['xl'],
+                height: 32,
+                borderRadius: BorderRadius['none'],
+                gap: Spacing['md'],
+              },
+              text: {
+                color: Colors.surface.onVariant,
+                ...StyleSheet.flatten(Font.label.large),
+              },
+              border: {
+                borderWidth: 1,
+                borderColor: Colors.surface.onVariant,
+              },
+            },
+          },
+        },
+      },
+    },
+    text: {
+      size: {
+        // Medium Button
+        medium: {
+          borderRadius: {
+            large: {
+              background: {
+                backgroundColor: 'transparent',
+                paddingHorizontal: Spacing['none'],
+                height: undefined,
+                borderRadius: BorderRadius['none'],
+                gap: Spacing['lg'],
+              },
+              text: {
+                color: Colors.surface.onVariant,
+                ...StyleSheet.flatten(Font.label.extraLargeProminent),
+              },
+              border: {
+
+              },
+            },
+            medium: {
+              background: {
+                backgroundColor: 'transparent',
+                paddingHorizontal: Spacing['none'],
+                height: undefined,
+                borderRadius: BorderRadius['none'],
+                gap: Spacing['lg'],
+              },
+              text: {
+                color: Colors.surface.onVariant,
+                ...StyleSheet.flatten(Font.label.extraLargeProminent),
+              },
+              border: {
+
+              },
+            },
+            small: {
+              background: {
+                backgroundColor: 'transparent',
+                paddingHorizontal: Spacing['none'],
+                height: undefined,
+                borderRadius: BorderRadius['none'],
+                gap: Spacing['lg'],
+              },
+              text: {
+                color: Colors.surface.onVariant,
+                ...StyleSheet.flatten(Font.label.extraLargeProminent),
+              },
+              border: {
+
+              },
+            },
+            none: {
+              background: {
+                backgroundColor: 'transparent',
+                paddingHorizontal: Spacing['none'],
+                height: undefined,
+                borderRadius: BorderRadius['none'],
+                gap: Spacing['lg'],
+              },
+              text: {
+                color: Colors.surface.onVariant,
+                ...StyleSheet.flatten(Font.label.extraLargeProminent),
+              },
+              border: {
+
+              },
+            },
+          },
+        },
+
+        // Small Button
+        small: {
+          borderRadius: {
+            large: {
+              background: {
+                backgroundColor: 'transparent',
+                paddingHorizontal: Spacing['none'],
+                height: undefined,
+                borderRadius: BorderRadius['none'],
+                gap: Spacing['md'],
+              },
+              text: {
+                color: Colors.surface.onVariant,
+                ...StyleSheet.flatten(Font.label.large),
+              },
+              border: {
+
+              },
+            },
+            medium: {
+              background: {
+                backgroundColor: 'transparent',
+                paddingHorizontal: Spacing['none'],
+                height: undefined,
+                borderRadius: BorderRadius['none'],
+                gap: Spacing['md'],
+              },
+              text: {
+                color: Colors.surface.onVariant,
+                ...StyleSheet.flatten(Font.label.large),
+              },
+              border: {
+
+              },
+            },
+            small: {
+              background: {
+                backgroundColor: 'transparent',
+                paddingHorizontal: Spacing['none'],
+                height: undefined,
+                borderRadius: BorderRadius['none'],
+                gap: Spacing['md'],
+              },
+              text: {
+                color: Colors.surface.onVariant,
+                ...StyleSheet.flatten(Font.label.large),
+              },
+              border: {
+
+              },
+            },
+            none: {
+              background: {
+                backgroundColor: 'transparent',
+                paddingHorizontal: Spacing['none'],
+                height: undefined,
+                borderRadius: BorderRadius['none'],
+                gap: Spacing['md'],
+              },
+              text: {
+                color: Colors.surface.onVariant,
+                ...StyleSheet.flatten(Font.label.large),
+              },
+              border: {
+
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+};
+
