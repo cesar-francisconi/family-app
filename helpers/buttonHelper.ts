173fe8365a999d1afa7d75b5261470fce12a9bb6
Cesar Francisconi <cesarafpiza@gmail.com>
2025-04-17 21:44:05 -0300

    modify getButtonToken to return ButtonStyleTokens

--- helpers/buttonHelper.ts
+++ helpers/buttonHelper.ts
@@ -1,11 +1,9 @@
-import { ButtonTokens, ButtonTokensType } from "@/constants/Button";
+import { ButtonTokens } from "@/constants/Button";
 import { ButtonProps } from "@/components/Button/types";
+import { ButtonStyleTokens } from "@/constants/Button/primary";
 
-type SafeBorderRadius = NonNullable<ButtonProps['borderRadius']>;
-
-type ButtonTokensTypeReturn = ButtonTokensType['type'][ButtonProps['type']]['variant'][ButtonProps['variant']]['size'][ButtonProps['size']]['borderRadius'][SafeBorderRadius]
-
-export function getButtonToken(props: ButtonProps): ButtonTokensTypeReturn {
+export function getButtonToken(props: ButtonProps): ButtonStyleTokens {
+    
     const { type = 'primary', variant = 'filled', size = 'medium', borderRadius = 'none' } = props;
 
     return ButtonTokens.type[type].variant[variant].size[size].borderRadius[borderRadius];
