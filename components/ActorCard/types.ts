dea1f2b760d2a3242ab704073e6afa9faf3e7d96
Cesar Francisconi <cesarafpiza@gmail.com>
2025-04-23 13:45:19 -0300

    add ActorNameProps[bgTransparent] for better type consistency

--- components/ActorCard/types.ts
+++ components/ActorCard/types.ts
@@ -1,8 +1,10 @@
+import { ActorNameProps } from "../ActorName/types";
+
 export type ActorCardProps = {
     id: string;
     name: string;
     imageUrl: string;
     stroke?: boolean;
-    actorNameBGTransparent?: boolean;
+    actorNameBGTransparent?: ActorNameProps['bgTransparent'];
     borderRadius?: 'none' | 'large' | 'medium' | 'small' | 'none';
 }
