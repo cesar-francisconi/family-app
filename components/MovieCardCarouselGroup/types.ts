19273fb888216ab69c9cddf4cdc31ad08383ffcf
Cesar Francisconi <cesarafpiza@gmail.com>
2025-04-23 14:02:45 -0300

    add MovieCardProps[showTitle] and MovieCardCarouselProps[buttonTitle] for better type consistency

--- components/MovieCardCarouselGroup/types.ts
+++ components/MovieCardCarouselGroup/types.ts
@@ -2,8 +2,8 @@ import { MovieCardProps } from "../MovieCard/types";
 import { MovieCardCarouselProps } from "../MovieCardCarousel/types";
 
 export type MovieCardCarouselGroupProps = {
-    showMovieCardTitle?: boolean;
-    movieCardCarouselButtonTitle?: string;
+    showMovieCardTitle?: MovieCardProps['showTitle'];
+    movieCardCarouselButtonTitle?: MovieCardCarouselProps['buttonTitle'];
     movieCardCarousels: {
         id: string;
         category: string;
