199f81ebc22cabb65c453f64482f42969317ad6a
Cesar Francisconi <cesarafpiza@gmail.com>
2025-04-23 13:49:05 -0300

    improves readability by extracting style variables

--- components/Actors/types.ts
+++ components/Actors/types.ts
@@ -1,9 +1,10 @@
 import { ActorCardProps } from "../ActorCard/types";
+import { ActorNameProps } from "../ActorName/types";
 
 export type ActorsCardProps = {
     data: ActorCardProps[];
     title?: string;
     showTitle?: boolean;
-    actorCardStroke?: boolean;
-    actorNameBGTransparent?: boolean;
+    actorCardStroke?: ActorCardProps['stroke'];
+    actorNameBGTransparent?: ActorNameProps['bgTransparent'];
 }
