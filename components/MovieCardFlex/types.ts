d04bc0e473cbb9ef5f904de9a14918e1dcc195e5
Cesar Francisconi <cesarafpiza@gmail.com>
2025-04-23 13:38:33 -0300

    add MovieCardFlex component with styles and types

---
+++ components/MovieCardFlex/types.ts
@@ -0,0 +1,7 @@
+export type MovieCardFlexProps = {
+    id: string;
+    title?: string;
+    showTitle?: boolean;
+    imageUrl: string;
+    borderRadius?: 'none' | 'small' | 'medium' | 'large',
+};
