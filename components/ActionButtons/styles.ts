6102431f57083b2d8555530365bdc716d03c5fad
Cesar Francisconi <cesarafpiza@gmail.com>
2025-04-27 11:49:27 -0300

    add ActionButtons component with styles and types

---
+++ components/ActionButtons/styles.ts
@@ -0,0 +1,9 @@
+import { Spacing } from "@/constants/Spacing";
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        gap: Spacing['lg'],
+    },
+});
