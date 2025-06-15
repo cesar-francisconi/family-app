5889e5a72ef90d80510d7f267cc5c7c25ef439e0
Cesar Francisconi <cesarafpiza@gmail.com>
2025-06-15 14:52:26 -0300

    add Details screen

---
+++ src/screen/Details/styles.ts
@@ -0,0 +1,41 @@
+import { detailsMovieCardHeight } from "@/src/app/(details)";
+import { headerHeight } from "@/src/components/Header";
+import { Colors } from "@/src/constants/Colors";
+import { Spacing } from "@/src/constants/Spacing";
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    mainContainer: {
+        flex: 1,
+        paddingTop: headerHeight,
+    },
+    contentContainerStyle: {
+        paddingBottom: Spacing['2xl'],
+    },
+    container: {
+        padding: Spacing['2xl'],
+        gap: Spacing['4xl'],
+    },
+    movieMainCardContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderBottomWidth: 1,
+        borderColor: Colors.surface.main,
+        zIndex: 999,
+    },
+    movieMainCard: {
+        width: '100%',
+    },
+    playCircleButton: {
+        position: 'absolute',
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingLeft: 5,
+        width: 70,
+        height: 70,
+        borderRadius: 999,
+        backgroundColor: Colors.primary.main,
+        borderWidth: 2,
+        borderColor: Colors.inverseSurface.on,
+    },
+});
---
+++ src/screen/Details/types.ts
@@ -0,0 +1,7 @@
+export type DetailsLocalSearchParams = {
+    movieId: string;
+};
+
+export type DetailsProps = {
+
+};
