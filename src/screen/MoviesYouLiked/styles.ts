7b5b92246dca3554add1c472a1e5cac44ee8e4af
Cesar Francisconi <cesarafpiza@gmail.com>
2025-06-15 13:56:55 -0300

    add MoviesYouLiked screen

---
+++ src/screen/MoviesYouLiked/styles.ts
@@ -0,0 +1,13 @@
+import { detailsMovieCardHeight } from "@/src/app/(details)";
+import { headerHeight } from "@/src/components/Header";
+import { Colors } from "@/src/constants/Colors";
+import { Spacing } from "@/src/constants/Spacing";
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: Colors.surface.main,
+        paddingTop: headerHeight,
+    },
+});
