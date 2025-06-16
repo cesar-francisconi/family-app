dfcbdf145db76585567ad6f2b615f62a8f8bf555
Cesar Francisconi <cesarafpiza@gmail.com>
2025-06-16 16:30:51 -0300

    add HeaderLeft component with styles and types

---
+++ src/components/HeaderLeft/index.tsx
@@ -0,0 +1,33 @@
+import { TouchableOpacity } from 'react-native';
+import { useRouter } from 'expo-router';
+import { Icon } from '../Icon';
+import { styles } from './styles';
+import {
+    AppRoutes,
+    usePathName,
+} from '@/src/hook/usePathname';
+
+export function HeaderLeft() {
+
+    const router = useRouter();
+    const pathname = usePathName();
+    
+    const handleBack = () => {
+        const backTimes: Partial<Record<AppRoutes, number>> = {
+            '/comment': 2,
+            '/answers': 3,
+        };
+
+        const times = backTimes[pathname] ?? 1;
+
+        for (let i = 0; i < times; i++) {
+            router.back();
+        };
+    };
+
+    return (
+        <TouchableOpacity activeOpacity={0.7} style={styles.container} onPress={handleBack}>
+            <Icon name="Entypo" icon="chevron-thin-left" size="medium" />
+        </TouchableOpacity>
+    );
+}
---
+++ src/components/HeaderLeft/styles.ts
@@ -0,0 +1,11 @@
+import { BorderRadius } from "@/src/constants/BorderRadius";
+import { Spacing } from "@/src/constants/Spacing";
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        padding: Spacing['lg'],
+        marginLeft: -Spacing['lg'],
+        borderRadius: BorderRadius['2xl'],
+    }
+});
