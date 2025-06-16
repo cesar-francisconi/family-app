fe7e1162590a4c82de5b215d54b4a7e9fc268dfd
Cesar Francisconi <cesarafpiza@gmail.com>
2025-06-16 16:28:32 -0300

    add ActivityIndicator component with styles and types

---
+++ src/components/ActivityIndicator/index.tsx
@@ -0,0 +1,21 @@
+import {
+    ActivityIndicator as AIndicator,
+    View,
+} from 'react-native';
+import { ActivityIndicatorProps } from './types';
+import { styles } from './styles';
+import { Colors } from '@/src/constants/Colors';
+
+export function ActivityIndicator(props: ActivityIndicatorProps) {
+
+    const {
+
+    } = props;
+
+    return (
+        <View style={styles.container}>
+            <AIndicator color={Colors.link} size={32} />
+        </View>
+    );
+}
+
