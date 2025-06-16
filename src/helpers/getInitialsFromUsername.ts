c689e2b12bcd6a5a09f36de6bd38d23d3481910e
Cesar Francisconi <cesarafpiza@gmail.com>
2025-06-16 20:27:57 -0300

    add getInitialsFromUsername helper to extract user initials

---
+++ src/helpers/getInitialsFromUsername.ts
@@ -0,0 +1,20 @@
+export function getInitialsFromUsername(username: string): string {
+    if (!username) return '';
+
+    let cleanName = username.startsWith('@') ? username.slice(1) : username;
+
+    cleanName = cleanName
+        .normalize('NFD')
+        .replace(/[\u0300-\u036f]/g, '')
+        .replace(/[^a-zA-Z]/g, '')
+
+    if (cleanName.length === 0) return '';
+
+    // Se só tiver uma letra
+    if (cleanName.length === 1) {
+        return cleanName[0].toUpperCase();
+    }
+
+    // Retorna as duas primeiras letras
+    return cleanName.slice(0, 2).toUpperCase();
+}
