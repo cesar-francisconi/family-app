0e2772961249a41aea53a9c287d8fa6760d73f67
Cesar Francisconi <cesarafpiza@gmail.com>
2025-07-01 18:45:33 -0300

    merge movie-related helpers into "useMovie.ts"

--- src/helpers/getActorMoviesById.ts
+++
@@ -1,9 +0,0 @@
-import { getAllMovies } from "@/src/hook/useMovie";
-
-export function getActorMoviesById(actorId: string) {
-    const movies = getAllMovies();
-
-    const actorMovies = movies.filter((movie) => movie.cast.some((actor) => actorId === actor.id));
-
-    return actorMovies;
-};
--- src/helpers/sortByPopularity.ts
+++
@@ -1,8 +0,0 @@
-import { Movie } from "@/movie";
-import { getAllMovies } from "../hook/useMovie";
-
-export function sortByPopularity(): Movie[] {
-    const movies = getAllMovies();
-
-    return [...movies].sort((a, b) => b.like - a.like);
-}
