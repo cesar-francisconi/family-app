74b8937404daee58b1c7cbdbd7887f907c277f51
Cesar Francisconi <cesarafpiza@gmail.com>
2025-07-01 20:13:45 -0300

    add EmailChange screen

---
+++ src/screen/EmailChange/styles.ts
@@ -0,0 +1,13 @@
+import { Colors } from "@/src/constants/Colors";
+import { Spacing } from "@/src/constants/Spacing";
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: Colors.surface.containerHigh,
+        paddingHorizontal: Spacing['2xl'],
+        paddingTop: Spacing['5xl'] + 80,
+        gap: Spacing['4xl'],
+    },
+});
