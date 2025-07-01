da794dc5fe1006d286243110e65faf7c5f95022e
Cesar Francisconi <cesarafpiza@gmail.com>
2025-07-01 19:07:04 -0300

    convert toggle functions to async with state management

--- src/components/ActionButtons/index.tsx
+++ src/components/ActionButtons/index.tsx
@@ -16,6 +16,7 @@ import {
 import {
     useCallback,
     useRef,
+    useState,
 } from 'react';
 import {
     useFocusEffect,
@@ -32,22 +33,36 @@ export function ActionButtons(props: ActionButtonsProps) {
     const animationLikeRef = useRef<LottieView>(null);
     const animationMyListRef = useRef<LottieView>(null);
 
+    const isReadyToggleLikeMovie = useRef(true);
+    const isReadyToggleMyList = useRef(true);
+
+    const [isMovieLikedState, setIsMovieLikedState] = useState(false);
+    const [isMovieInMyListState, setIsMovieInMyListState] = useState(false);
+
     useFocusEffect(
         useCallback(() => {
-            const loadMovieStatus = () => {
-                const liked = isMovieLiked(id);
-                const inList = isMovieInMyList(id);
+            const loadMovieStatus = async () => {
+                const liked = await isMovieLiked(id);
+                const inList = await isMovieInMyList(id);
 
                 if (liked) {
                     animationLikeRef.current?.play(130, 130);
+
+                    setIsMovieLikedState(true);
                 } else {
                     animationLikeRef.current?.play(0, 0);
+
+                    setIsMovieLikedState(false);
                 };
 
                 if (inList) {
                     animationMyListRef.current?.play(15, 15);
+
+                    setIsMovieInMyListState(true);
                 } else {
                     animationMyListRef.current?.play(0, 0);
+
+                    setIsMovieInMyListState(false);
                 };
             };
 
@@ -55,26 +70,46 @@ export function ActionButtons(props: ActionButtonsProps) {
         }, [id])
     );
 
-    const toggleLikeMovie = () => {
-        toggleItemStatus({
-            id,
-            checkFn: isMovieLiked,
-            addFn: addToLikedMovies,
-            removeFn: removeFromLikedMovies,
-        });
 
-        isMovieLiked(id) ? animationLikeRef.current?.play(30, 130) : animationLikeRef.current?.play(0, 0);
+    const toggleLikeMovie = async () => {
+        if (isReadyToggleLikeMovie.current) {
+            isReadyToggleLikeMovie.current = false;
+
+            !isMovieLikedState ? animationLikeRef.current?.play(30, 130) : animationLikeRef.current?.play(0, 0);
+
+            await toggleItemStatus({
+                id,
+                checkFn: isMovieLiked,
+                addFn: addToLikedMovies,
+                removeFn: removeFromLikedMovies,
+            });
+
+            const result = await isMovieLiked(id);
+            setIsMovieLikedState(result);
+
+            isReadyToggleLikeMovie.current = true;
+        };
     };
 
-    const toggleMyList = () => {
-        toggleItemStatus({
-            id,
-            checkFn: isMovieInMyList,
-            addFn: addToMyList,
-            removeFn: removeFromMyList,
-        });
+    const toggleMyList = async () => {
+        if (isReadyToggleMyList.current) {
+            isReadyToggleMyList.current = false;
 
-        isMovieInMyList(id) ? animationMyListRef.current?.play(0, 15) : animationMyListRef.current?.play(0, 0);
+            !isMovieInMyListState ? animationMyListRef.current?.play(0, 15) : animationMyListRef.current?.play(0, 0);
+
+            await toggleItemStatus({
+                id,
+                checkFn: isMovieInMyList,
+                addFn: addToMyList,
+                removeFn: removeFromMyList,
+            });
+
+
+            const result = await isMovieInMyList(id);
+            setIsMovieInMyListState(result);
+
+            isReadyToggleMyList.current = true;
+        };
     };
 
     return (
