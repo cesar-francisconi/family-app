301c60450636c5e97a910cb1c028611d34e445f0
Cesar Francisconi <cesarafpiza@gmail.com>
2025-07-01 20:23:57 -0300

    add submitCommentOrAnswer to handle comment and answer submission and editing

---
+++ src/helpers/submitCommentOrAnswer.ts
@@ -0,0 +1,112 @@
+import { TextInput } from "react-native";
+import {
+    changeAnswerById,
+    changeCommentById,
+    setAnswer,
+    setComment,
+} from "../hook/useMovie";
+import BottomSheet from "@gorhom/bottom-sheet";
+import {
+    CommentReplySheetOptions,
+    setCommentReplySheet,
+} from "../hook/useCommentReplySheet";
+import { AppRoutes } from "../hook/usePathname";
+import { getAuth } from "@react-native-firebase/auth";
+import {
+    doc,
+    getDoc,
+    getFirestore,
+} from "@react-native-firebase/firestore";
+import { User } from "../hook/useUser";
+
+interface SubmitCommentOrAnswerProps {
+    movieId: string;
+    inputValue: string;
+    pathname: AppRoutes;
+    inputRef: React.RefObject<TextInput | null>;
+    commentReplySheetOptions: CommentReplySheetOptions,
+    bottomSheetRef: React.RefObject<BottomSheet | null>;
+    resetInput: () => void;
+};
+
+export const submitCommentOrAnswer = async (props: SubmitCommentOrAnswerProps) => {
+
+    const {
+        movieId,
+        inputValue,
+        pathname,
+        inputRef,
+        commentReplySheetOptions,
+        bottomSheetRef,
+        resetInput,
+    } = props;
+
+    const auth = getAuth();
+    const user = auth.currentUser;
+    const isComment = pathname === '/comment';
+    const isAnswers = pathname === '/answers';
+
+    if (!(user && user.uid)) return;
+
+    const uid = user.uid;
+    const db = getFirestore();
+    const userRef = doc(db, 'users', uid as string);
+    const userSnap = await getDoc(userRef);
+
+    const data = userSnap.data() as Omit<User, 'id'>;
+    
+    if (!data) {
+        console.error(`User com ID ${uid} não encontrado.`);
+        return;
+    };
+
+    const username = data.username;
+    const background = data.background;
+    const avatar = data.avatar;
+    const userId = uid;
+
+    if (commentReplySheetOptions.origin === 'isCommentAuthorizedUserActionsSheetEdit' || commentReplySheetOptions.origin === 'isSelectedParentCommentAuthorizedUserActionsSheetEdit') {
+        changeCommentById(inputValue);
+
+        bottomSheetRef.current?.close();
+        inputRef.current?.blur();
+        setCommentReplySheet({ isOpen: false });
+        resetInput();
+
+        return;
+    };
+
+    if (commentReplySheetOptions.origin === 'isAnswerAuthorizedUserActionsSheetEdit') {
+        changeAnswerById(inputValue);
+
+        bottomSheetRef.current?.close();
+        inputRef.current?.blur();
+        setCommentReplySheet({ isOpen: false });
+        resetInput();
+
+        return;
+    };
+
+    if (isComment) {
+        setComment(movieId, {
+            username,
+            avatar,
+            background,
+            userId,
+            comment: inputValue,
+        });
+    } else if (isAnswers) {
+        setAnswer(movieId, {
+            username,
+            avatar,
+            background,
+            userId,
+            answer: inputValue,
+        });
+    };
+
+    bottomSheetRef.current?.close();
+    inputRef.current?.blur();
+    setCommentReplySheet({ isOpen: false });
+    resetInput();
+};
