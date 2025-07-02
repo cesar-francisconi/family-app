175b5ce9950f26d988ce260d7b1015e42c671d0a
Cesar Francisconi <cesarafpiza@gmail.com>
2025-07-02 20:14:33 -0300

    refactor ChipsCarouselGroup to load filters asynchronously with loading indicator

--- src/components/ChipsCarouselGroup/index.tsx
+++ src/components/ChipsCarouselGroup/index.tsx
@@ -6,16 +6,19 @@ import { ChipsCarouselGroupProps } from './types';
 import { ChipsCarousel } from '../ChipsCarousel';
 import {
     extractChips,
+    Filters,
     QuartiaryOption,
 } from '@/src/helpers/extractChips';
 import {
     useEffect,
     useState,
 } from 'react';
-import { filterMovies } from '@/src/helpers/filterMovies';
+import { ActivityIndicator } from '../ActivityIndicator';
+import { filterMovies } from '@/src/hook/useMovie';
 
 export function ChipsCarouselGroup(props: ChipsCarouselGroupProps) {
 
+    const [chips, setChips] = useState<Filters | null>(null);
     const [actor, setActor] = useState('Tudo');
     const [genre, setGenre] = useState('Tudo');
     const [year, setYear] = useState('Tudo');
@@ -25,11 +28,20 @@ export function ChipsCarouselGroup(props: ChipsCarouselGroupProps) {
         chipOptions,
     } = props;
 
-    const chips = extractChips();
+    useEffect(() => {
+        (async () => {
+            const chips = await extractChips();
+
+            setChips(chips);
+        })();
+    }, []);
+
 
     useEffect(() => {
         filterMovies({ actor, genre, year, order });
     }, [actor, genre, year, order]);
+
+    if (!chips) return <ActivityIndicator />;
 
     return (
         <View
