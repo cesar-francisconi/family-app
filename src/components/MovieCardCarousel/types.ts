5e71e025c0761e73681c3e79670a19f707d8d426
Cesar Francisconi <cesarafpiza@gmail.com>
2025-07-02 20:45:47 -0300

    refactor MovieCardCarouselProps: remove unused props, introduce movieCardOptions and global search param types

--- src/components/MovieCardCarousel/types.ts
+++ src/components/MovieCardCarousel/types.ts
@@ -1,11 +1,16 @@
 import { CategoryParam } from "@/movie";
-import { ButtonProps } from "../Button/types";
-import { MovieCardProps } from "../MovieCard/types";
+import {
+    MovieCardProps,
+    MovieCardPropsDefault,
+} from "../MovieCard/types";
+
+export type MovieCardCarouselGlobalSearchParams = {
+    actorId: string;
+    actorName: string;
+};
 
 export type MovieCardCarouselProps = {
     category: CategoryParam;
-    actorId?: string;
-    buttonTitle?: ButtonProps['title'];
     movies: MovieCardProps[];
-    withMovieCardTitle?: MovieCardProps['withTitle'];
+    movieCardOptions?: Pick<MovieCardPropsDefault, 'withTitle'>,
 };
