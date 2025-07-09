8d92b0c0b2428cc5a9a4d8c7529138222d7b9b45
Cesar Francisconi <cesarafpiza@gmail.com>
2025-07-09 19:41:20 -0300

    removed marginTop style from contentStyle to fit new animation style

--- src/app/(app)/(details)/_layout.tsx
+++ src/app/(app)/(details)/_layout.tsx
@@ -36,9 +36,6 @@ export default function RootLayoutNav() {
                 options={{
                     presentation: 'transparentModal',
                     animation: 'fade',
-                    contentStyle: {
-                        marginTop: detailsMovieCardHeight + headerHeight,
-                    }
                 }}
             />
         </Stack >
