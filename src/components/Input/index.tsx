24c58468ef44ec706a4afc8572a84b7033569d40
Cesar Francisconi <cesarafpiza@gmail.com>
2025-07-15 18:00:27 -0300

    integrate external input state helper and cleanup token logic

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,4 +1,8 @@
-import React, { forwardRef } from 'react';
+import React, {
+    forwardRef,
+    useRef,
+    useState,
+} from 'react';
 import {
     Text,
     TextInput,
@@ -11,8 +15,9 @@ import { LabelCheck } from '../LabelCheck';
 import { Colors } from '@/src/constants/Colors';
 import { getInputToken } from '@/src/helpers/getInputToken';
 import { Controller } from 'react-hook-form';
+import { determineInputStateAndUpdateToken } from '@/src/helpers/determineInputStateAndUpdateToken';
 
-export const Input = forwardRef<TextInput, InputProps>((props, ref) => {
+export const Input = forwardRef<TextInput, Omit<InputProps, 'state'>>((props, ref) => {
 
     const {
         name,
@@ -36,7 +41,10 @@ export const Input = forwardRef<TextInput, InputProps>((props, ref) => {
         ...inputProps
     } = props;
 
-    const token = getInputToken(props);
+    const [isFocused, setIsFocused] = useState(false);
+    const IsValidating = useRef(false);
+
+    let token = getInputToken({ ...props, state: 'default' });
 
     const justifyContent = withHelpMessage ? 'space-between' : 'flex-end';
 
@@ -46,71 +54,86 @@ export const Input = forwardRef<TextInput, InputProps>((props, ref) => {
             name={name}
             render={({
                 field: { onChange, onBlur, value },
-                fieldState: { error },
-            }) => (
-                <View style={[styles.mainContainer, token.mainContainer, style]}>
-                    {withLabel && (
-                        <Text style={[styles.label, token.label]}>
-                            {label}
-                        </Text>
-                    )}
+                fieldState: { error, invalid, isDirty, isValidating },
+            }) => {
+                token = determineInputStateAndUpdateToken({
+                    isFocused,
+                    isDirty,
+                    invalid,
+                    isValidating,
+                    isValidatingRef: IsValidating,
+                    props: { ...props, state: 'default' },
+                });
 
-                    <View style={[styles.container, token.container]}>
-                        <View style={[styles.leftIconAndInput, token.leftIconAndInput]}>
-                            {leftIcon && (
-                                <TouchableOpacity onPress={fnLeftIcon} disabled={!fnLeftIcon}>
-                                    {React.cloneElement(leftIcon, token.icon)}
+                return (
+                    <View style={[styles.mainContainer, token.mainContainer, style]}>
+                        {withLabel && (
+                            <Text style={[styles.label, token.label]}>
+                                {label}
+                            </Text>
+                        )}
+
+                        <View style={[styles.container, token.container]}>
+                            <View style={[styles.leftIconAndInput, token.leftIconAndInput]}>
+                                {leftIcon && (
+                                    <TouchableOpacity onPress={fnLeftIcon} disabled={!fnLeftIcon}>
+                                        {React.cloneElement(leftIcon, token.icon)}
+                                    </TouchableOpacity>
+                                )}
+
+                                <TextInput
+                                    ref={ref}
+                                    value={value}
+                                    onChangeText={onChange}
+                                    onBlur={() => {
+                                        onBlur();
+                                        setIsFocused(false);
+                                    }}
+                                    onFocus={() => setIsFocused(true)}
+                                    placeholder={placeholder}
+                                    placeholderTextColor={token.placeholderTextColor}
+                                    style={[styles.input, token.input]}
+                                    {...inputProps}
+                                />
+                            </View>
+
+                            {rightIcon && (
+                                <TouchableOpacity onPress={fnRightIcon} disabled={!fnRightIcon}>
+                                    {React.cloneElement(rightIcon, token.icon)}
                                 </TouchableOpacity>
                             )}
-
-                            <TextInput
-                                ref={ref}
-                                value={value}
-                                onChangeText={onChange}
-                                onBlur={onBlur}
-                                placeholder={placeholder}
-                                placeholderTextColor={token.placeholderTextColor}
-                                style={[styles.input, token.input]}
-                                {...inputProps}
-                            />
                         </View>
 
-                        {rightIcon && (
-                            <TouchableOpacity onPress={fnRightIcon} disabled={!fnRightIcon}>
-                                {React.cloneElement(rightIcon, token.icon)}
-                            </TouchableOpacity>
+                        {withHelpMessageAndLabelCheck && (
+                            <View
+                                style={[
+                                    styles.helpTextContainer,
+                                    token.helpTextContainer,
+                                    { justifyContent },
+                                ]}
+                            >
+                                {withHelpMessage && (
+                                    <TouchableOpacity onPress={fnHelpMessage} disabled={!fnHelpMessage}>
+                                        <Text style={[styles.helpText, token.helpText, { color: helpMessageColor }]}>
+                                            {helpMessage}
+                                        </Text>
+                                    </TouchableOpacity>
+                                )}
+
+                                {withLabelCheck && (
+                                    <LabelCheck label={labelCheckLabel} isChecked={isCheckedLabelCheck} />
+                                )}
+                            </View>
+                        )}
+
+                        {error && (
+                            <Text style={[styles.error, token.error]}>
+                                {error.message}
+                            </Text>
                         )}
                     </View>
-
-                    {withHelpMessageAndLabelCheck && (
-                        <View
-                            style={[
-                                styles.helpTextContainer,
-                                token.helpTextContainer,
-                                { justifyContent },
-                            ]}
-                        >
-                            {withHelpMessage && (
-                                <TouchableOpacity onPress={fnHelpMessage} disabled={!fnHelpMessage}>
-                                    <Text style={[styles.helpText, token.helpText, { color: helpMessageColor }]}>
-                                        {helpMessage}
-                                    </Text>
-                                </TouchableOpacity>
-                            )}
-
-                            {withLabelCheck && (
-                                <LabelCheck label={labelCheckLabel} isChecked={isCheckedLabelCheck} />
-                            )}
-                        </View>
-                    )}
-
-                    {error && (
-                        <Text style={[styles.error, token.error]}>
-                            {error.message}
-                        </Text>
-                    )}
-                </View>
-            )}
+                )
+            }}
         />
     );
 });
