3589cc5dc6f513c12c4b9b754e9feaf6ebd62a70
Cesar Francisconi <cesarafpiza@gmail.com>
2025-07-19 17:43:51 -0300

    implement Zustand store to manage ConfirmDeleteCommentModal visibility and parameters

---
+++ src/hook/useConfirmDeleteCommentModal.ts
@@ -0,0 +1,33 @@
+import { create } from 'zustand';
+import { AuthorizedUserActionsSheetOptions } from './useAuthorizedUserActionsSheet';
+import { BottomSheetMethods } from '@gorhom/bottom-sheet/lib/typescript/types';
+
+export interface ConfirmDeleteCommentModalOptions {
+    isOpen: boolean;
+    param?: {
+        bottomSheetRef: React.RefObject<BottomSheetMethods | null>;
+        origin: AuthorizedUserActionsSheetOptions['origin'];
+    };
+};
+
+interface InitialStateProps {
+    options: ConfirmDeleteCommentModalOptions;
+};
+
+const initialState: InitialStateProps = {
+    options: {
+        isOpen: false,
+        param: undefined,
+    },
+};
+
+export const useConfirmDeleteCommentModal = create(() => initialState);
+
+export const setConfirmDeleteCommentModal = ({ isOpen, param }: ConfirmDeleteCommentModalOptions) => useConfirmDeleteCommentModal.setState(() => {
+    return {
+        options: {
+            isOpen,
+            param,
+        },
+    };
+});
