e1d5542f2547435e16da414ba213f67731f74950
Cesar Francisconi <cesarafpiza@gmail.com>
2025-08-16 14:02:03 -0300

    simplify toast component by removing Pressable and OK buttons

--- src/helpers/toastConfig.tsx
+++ src/helpers/toastConfig.tsx
@@ -1,85 +1,49 @@
 import {
-  Pressable,
   StyleSheet,
   Text,
   View,
 } from "react-native";
-import Toast, {
+import {
   BaseToastProps,
 } from "react-native-toast-message"
-import { getScreenHeight } from "./getScreenHeight";
 import { Spacing } from "../constants/Spacing";
 import { Colors } from "../constants/Colors";
 import { Font } from "../constants/Font";
 import { BorderRadius } from "../constants/BorderRadius";
 
 export const toastConfig = {
-  customSuccessBase: ({ text2 }: BaseToastProps) => (
-    <Pressable
-      style={styles.toastMainContainer}
-      onPress={() => Toast.hide()}
-    >
-      <View style={[styles.toastContainer, styles.successContainer]}>
-        <Text style={[styles.message, {
-          marginBottom: 0,
-        }]}>{text2}</Text>
-      </View>
-    </Pressable>
-  ),
-
   customSuccess: ({ text1, text2 }: BaseToastProps) => (
-    <Pressable
+    <View
       style={styles.toastMainContainer}
-      onPress={() => Toast.hide()}
     >
       <View style={[styles.toastContainer, styles.successContainer]}>
-        <Text style={styles.title}>{text1}</Text>
-        <Text style={styles.message}>{text2}</Text>
-        <Pressable
-          onPress={() => Toast.hide()}
-          style={[styles.button, styles.successButton]}
-        >
-          <Text style={styles.buttonText}>OK</Text>
-        </Pressable>
+        {text1 && <Text style={styles.title}>{text1}</Text>}
+        {text2 && <Text style={styles.message}>{text2}</Text>}
       </View>
-    </Pressable>
+    </View>
   ),
 
   customInfo: ({ text1, text2 }: BaseToastProps) => (
-    <Pressable
+    <View
       style={styles.toastMainContainer}
-      onPress={() => Toast.hide()}
     >
       <View style={[styles.toastContainer, styles.infoContainer]}>
-        <Text style={styles.title}>{text1}</Text>
-        <Text style={styles.message}>{text2}</Text>
-        <Pressable
-          onPress={() => Toast.hide()}
-          style={[styles.button, styles.infoButton]}
-        >
-          <Text style={styles.buttonText}>OK</Text>
-        </Pressable>
+        {text1 && <Text style={styles.title}>{text1}</Text>}
+        {text2 && <Text style={styles.message}>{text2}</Text>}
       </View>
-    </Pressable>
+    </View>
   ),
 
 
   customError: ({ text1, text2 }: BaseToastProps) => (
-    <Pressable
+    <View
       style={styles.toastMainContainer}
-      onPress={() => Toast.hide()}
     >
       <View style={[styles.toastContainer, styles.errorContainer]}>
-        <Text style={styles.title}>{text1}</Text>
-        <Text style={styles.message}>{text2}</Text>
-        <Pressable
-          onPress={() => Toast.hide()}
-          style={[styles.button, styles.errorButton]}
-        >
-          <Text style={styles.buttonText}>OK</Text>
-        </Pressable>
+        {text1 && <Text style={styles.title}>{text1}</Text>}
+        {text2 && <Text style={styles.message}>{text2}</Text>}
       </View>
-    </Pressable>
+    </View>
   ),
 };
 
