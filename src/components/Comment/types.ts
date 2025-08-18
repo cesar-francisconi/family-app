2598cb833e9cfb135b02e19352395c289a8e61f8
Cesar Francisconi <cesarafpiza@gmail.com>
2025-08-18 16:42:56 -0300

    add onMorePress and handleAnswersPress props

--- src/components/Comment/types.ts
+++ src/components/Comment/types.ts
@@ -3,6 +3,8 @@ import { Comment } from "@/movie";
 export type UserCommentPropsDefault = {
     answersText?: string;
     withAnswersText?: boolean;
+    onMorePress: () => void;
+    handleAnswersPress?: () => void;
 };
 
 export type UserCommentProps = Comment & UserCommentPropsDefault;
