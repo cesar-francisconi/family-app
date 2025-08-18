fd5686af13a667c78f184b94fc63b7542198c12f
Cesar Francisconi <cesarafpiza@gmail.com>
2025-08-18 16:36:56 -0300

    update styles with answers container, loading overlay, and container adjustments

--- src/screen/Answers/styles.ts
+++ src/screen/Answers/styles.ts
@@ -5,19 +5,31 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
+        borderTopWidth: 1,
+        borderColor: Colors.outline.variant,
+        backgroundColor: Colors.surface.container,
+    },
+    answersContainer: {
+        flex: 1,
     },
     selectedParentCommentContainer: {
         padding: Spacing['2xl'],
+        paddingBottom: 0,
         backgroundColor: Colors.surface.containerExtraHigh,
     },
     contentContainerStyle: {
-        paddingLeft: 32 + Spacing['2xl'], // 32 é o tamanho do avatar (width); e 16 é o paddingHorizontal da tela. Assim a base left fica ajustado exatamente no canto esquerdo do avatar do comentário.
-        padding: Spacing['2xl'],
         gap: Spacing['5xl'],
     },
     commentReplyFieldContainer: {
         padding: Spacing['3xl'],
         borderTopWidth: 1,
         borderColor: Colors.outline.variant,
+    },
+    loadingOverlay: {
+        position: 'absolute',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100%',
     },
 });
