be138dfdb849e94ddcd974ab8c1aa06f72bc0ee8
Cesar Francisconi <cesarafpiza@gmail.com>
2025-08-18 22:53:23 -0300

    unify debounce usage and update route
    - remove hardcoded 1000ms debounce, use default from hook
    - update navigation route from comment → comments
    - add 1000ms delay to navigation for consistency

--- src/components/AddCommentWithAvatar/index.tsx
+++ src/components/AddCommentWithAvatar/index.tsx
@@ -24,7 +24,7 @@ export function AddCommentWithAvatar(props: AddCommentWithAvatarProps) {
 
     const { movieId } = useGlobalSearchParams<AddCommentWithAvatarGlobalSearchParams>();
 
-    const { debounce } = useDebounce(1000);
+    const { debounce } = useDebounce();
 
     const route = useRouter();
 
@@ -40,7 +40,7 @@ export function AddCommentWithAvatar(props: AddCommentWithAvatarProps) {
 
             <AddComment
                 {...addCommentOptions}
-                fnAddComment={() => debounce(() => route.push(`/(app)/(details)/(comments)/comment?movieId=${movieId}&origin=isAddComment`))}
+                fnAddComment={() => debounce(() => route.push(`/(app)/(details)/(comments)/comments?movieId=${movieId}&origin=isAddComment`), 1000)}
             />
         </View>
     );
