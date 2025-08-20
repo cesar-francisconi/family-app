89dcb4e03a6897d358d835aa75c499fafa40d323
Cesar Francisconi <cesarafpiza@gmail.com>
2025-08-20 20:23:23 -0300

    rename CommentProps to CommentActionRowProps and add fnCommentActionRowPress

--- src/components/CommentActionRow/types.ts
+++ src/components/CommentActionRow/types.ts
@@ -1,9 +1,10 @@
 import { AddCommentProps } from "../AddComment/types";
 import { AvatarProps } from "../Avatar/types";
 
-export type CommentProps = {
+export type CommentActionRowProps = {
     avatarOptions: AvatarProps,
     addCommentOptions: AddCommentProps,
+    fnCommentActionRowPress?: () => void;
     withTitle?: boolean;
     title?: string;
     borderRadius?: 'none' | 'small' | 'medium' | 'large';
