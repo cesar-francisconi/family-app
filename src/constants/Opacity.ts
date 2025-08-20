25c5efaf8a856d1ab921674b730c5f8b887cd36f
Cesar Francisconi <cesarafpiza@gmail.com>
2025-08-19 22:12:25 -0300

    add Opacity constants mapping

---
+++ src/constants/Opacity.ts
@@ -0,0 +1,10 @@
+import { numberScale } from "./numberScale";
+
+export const Opacity = {
+    'none': numberScale['0'],
+    'xs': numberScale['0.5'],
+    'sm': numberScale['0.75'],
+    '2xl': numberScale['1'],
+};
+
+export const ActionDefaultOpacity = Opacity['sm'];
