39749612b96927b601b01cb84bb48f5ca2685f40
Cesar Francisconi <cesarafpiza@gmail.com>
2025-08-19 22:21:26 -0300

    rename AddComment text style to placeholder for clarity

--- src/components/AddComment/styles.ts
+++ src/components/AddComment/styles.ts
@@ -2,16 +2,10 @@ import { Colors } from "@/src/constants/Colors";
 import { Font } from "@/src/constants/Font";
 import { Spacing } from "@/src/constants/Spacing";
 import { StyleSheet } from "react-native";
-import { addCommentHeight } from ".";
 
 export const styles = StyleSheet.create({
     container: {
-        //flex: 1,
-        //paddingHorizontal: Spacing['lg'],
-        //paddingVertical: Spacing['sm'],
         justifyContent: 'center',
-        //: Colors.surface.containerExtraHigh,
-
         backgroundColor: Colors.surface.containerExtraHigh,
         flex: 1,
         borderRadius: 999,
@@ -19,7 +13,7 @@ export const styles = StyleSheet.create({
         color: Colors.surface.onVariant,
         borderColor: Colors.surface.on,
     },
-    text: {
+    placeholder: {
         ...StyleSheet.flatten(Font.label.large),
         color: Colors.surface.onVariant,
     },
