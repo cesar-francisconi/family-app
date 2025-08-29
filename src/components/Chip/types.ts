68e40bf8424755345b0626ac96f87a71ffc52d24
Cesar Francisconi <cesarafpiza@gmail.com>
2025-08-28 22:36:57 -0300

    update ChipProps to include textStyle and fnChipPress callback

--- src/components/Chip/types.ts
+++ src/components/Chip/types.ts
@@ -4,5 +4,6 @@ export type ChipProps = {
     text: string;
     isActive?: boolean;
     borderRadius?: 'none' | 'small' | 'medium' | 'large';
-    textTransform?: Required<TextStyle>['textTransform'];
+    textStyle?: TextStyle;
+    fnChipPress?: () => void;
 };
