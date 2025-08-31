fe9692d623b4519806a63830fcdc4232ab3ab7b1
Cesar Francisconi <cesarafpiza@gmail.com>
2025-08-30 23:14:23 -0300

    replace individual button props with unified buttonOptions in CommentReplyFieldProps

--- src/components/CommentReplyField/types.ts
+++ src/components/CommentReplyField/types.ts
@@ -1,7 +1,11 @@
 import { TextInputProps } from "react-native";
+import { ButtonPropsExtended } from "../Button/types";
 
 export interface CommentReplyFieldProps extends TextInputProps {
-    fnButton?: () => void;
-    buttonDisabled?: boolean;
-    isButtonLoading?: boolean;
+    buttonOptions?: {
+        onPress?: ButtonPropsExtended['onPress'];
+        disabled?: ButtonPropsExtended['disabled'];
+        isLoading?: ButtonPropsExtended['isLoading'];
+        leftIcon?: ButtonPropsExtended['leftIcon'];
+    };
 };
