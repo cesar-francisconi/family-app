122b7c32a8e88185c5ef57dc319739dbdd5e5fce
Cesar Francisconi <cesarafpiza@gmail.com>
2025-09-04 23:30:33 -0300

    replace button helpers with single resolver

--- src/helpers/getButtonIconSize.ts
+++
@@ -1,5 +0,0 @@
-import { ButtonPropsExtended } from "../components/Button/types";
-
-export function getButtonIconSize(size: ButtonPropsExtended['size']) {
-    return size === 'medium' ? 'small' : 'extraSmall';
-};
