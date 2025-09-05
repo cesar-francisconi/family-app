cb8dee147aa24bbf3936cb72a75d778fdb5f1499
Cesar Francisconi <cesarafpiza@gmail.com>
2025-09-04 23:28:23 -0300

    replace chip helpers with single resolver

--- src/helpers/resolveChipBackgroundColor.ts
+++
@@ -1,6 +0,0 @@
-import { ChipProps } from "../components/Chip/types";
-import { Colors } from "../constants/Colors";
-
-export function resolveChipBackgroundColor(isActive: ChipProps['isActive']) {
-    return isActive ? Colors.surface.containerHigh : undefined;
-};
--- src/helpers/resolveChipTextColor.ts
+++
@@ -1,6 +0,0 @@
-import { ChipProps } from "../components/Chip/types";
-import { Colors } from "../constants/Colors";
-
-export function resolveChipTextColor(isActive: ChipProps['isActive']) {
-    return isActive ? Colors.surface.on : Colors.surface.onVariant;
-};
