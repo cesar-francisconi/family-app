87c2a8478265973356fb897035d223c77e92f680
Cesar Francisconi <cesarafpiza@gmail.com>
2025-09-15 15:38:04 -0300

    remove VerticalButtonGroup component

--- src/components/VerticalButtonGroup/styles.ts
+++
@@ -1,8 +0,0 @@
-import { Spacing } from "@/src/constants/Spacing";
-import { StyleSheet } from "react-native";
-
-export const styles = StyleSheet.create({
-    container: {
-        gap: Spacing['2xl'],
-    },
-});
--- src/components/VerticalButtonGroup/types.ts
+++
@@ -1,4 +0,0 @@
-export type VerticalButtonGroupProps = {
-    firstButton: JSX.Element;
-    secondButton: JSX.Element;
-};
