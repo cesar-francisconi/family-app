db5c345cf842260ab2aa7d1c2a66266fb68a4ae9
Cesar Francisconi <cesarafpiza@gmail.com>
2025-09-15 17:15:39 -0300

    update style

--- src/components/Search/styles.ts
+++ src/components/Search/styles.ts
@@ -7,20 +7,11 @@ export const styles = StyleSheet.create({
     container: {
         flexDirection: 'row',
         backgroundColor: Colors.surface.containerHigh,
-        paddingLeft: Spacing['2xl'],
-        height: 50,
-        gap: Spacing['2xl'],
-    },
-    leftIconAndInputContainer: {
-        flexDirection: 'row',
-        gap: Spacing['2xl'],
-        flex: 1,
-        alignItems: 'center',
-        overflow: 'hidden',
     },
     input: {
         flex: 1,
         ...StyleSheet.flatten(Font.label.extraLarge),
         color: Colors.surface.on,
+        paddingHorizontal: Spacing['2xl'],
     },
 });
