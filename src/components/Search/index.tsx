88ddf62d23880c1855951cff92e4bbf788b415e4
Cesar Francisconi <cesarafpiza@gmail.com>
2025-09-15 17:18:03 -0300

    integrate react-hook-form Controller in Search, add buttonOptions prop with default async onPress, and memoize component

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -4,35 +4,63 @@ import {
 } from 'react-native';
 import { styles } from './styles';
 import { SearchProps } from './types';
-import { forwardRef } from 'react';
+import {
+    forwardRef,
+    memo,
+    useImperativeHandle,
+    useRef,
+} from 'react';
 import { Colors } from '@/src/constants/Colors';
 import { Button } from '../Button';
+import {
+    Controller,
+} from 'react-hook-form';
 
-export const Search = forwardRef<TextInput, SearchProps>((props, ref) => {
+const SearchBase = forwardRef<TextInput, SearchProps>((props, ref) => {
 
     const {
-        fnButton,
-        buttonTitle = 'Title',
-        buttonDisabled = true,
+        control,
+        name,
+        buttonOptions = { title: 'Title', onPress: async () => { } },
     } = props;
 
-    return (
-        <View style={styles.container}>
-            <TextInput
-                ref={ref}
-                placeholderTextColor={Colors.surface.onVariant}
-                style={styles.input}
-                {...props}
-            />
+    const inputRef = useRef<TextInput>(null);
 
-            <Button
-                onPress={fnButton}
-                title={buttonTitle}
-                type='primary'
-                disabled={buttonDisabled}
-                variant='filled'
-            />
-        </View>
+    useImperativeHandle(ref, () => inputRef.current!);
+
+    return (
+        <Controller
+            control={control}
+            name={name}
+            render={({
+                field: { onChange, onBlur, value },
+            }) => {
+                const trimmedInput = value.trim();
+                const buttonDisabled = trimmedInput === '';
+
+                return (
+                    <View style={styles.container}>
+                        <TextInput
+                            ref={inputRef}
+                            value={value}
+                            onChangeText={onChange}
+                            onBlur={onBlur}
+                            placeholderTextColor={Colors.surface.onVariant}
+                            style={styles.input}
+                            {...props}
+                        />
+
+                        <Button
+                            onPress={buttonOptions.onPress}
+                            title={buttonOptions.title}
+                            type='primary'
+                            disabled={buttonDisabled}
+                            variant='filled'
+                        />
+                    </View>
+                );
+            }} />
     );
 });
 
+export const Search = memo(SearchBase);
