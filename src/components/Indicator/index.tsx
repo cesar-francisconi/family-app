c3ab683762fdbb9a71471d385887dad89129fe3f
Cesar Francisconi <cesarafpiza@gmail.com>
2025-09-15 18:01:05 -0300

    memoize Indicator component with React.memo

--- src/components/Indicator/index.tsx
+++ src/components/Indicator/index.tsx
@@ -1,7 +1,8 @@
+import React from "react";
 import { View } from "react-native";
 import { styles } from "./styles";
 
-export function Indicator() {
+export const Indicator = React.memo(() => {
 
     return (
         <View
@@ -10,7 +11,7 @@ export function Indicator() {
             <View
                 style={styles.indicator}
             />
-        </View>
+        </View >
     );
-}
+});
 
